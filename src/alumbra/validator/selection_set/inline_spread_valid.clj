3149758243c77aa86e45b8765fe4df6e7e95b3cb
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-11-06 18:06:26 +0100

    adjust inline spread invariant.

--- src/alumbra/validator/selection_set/inline_spread_valid.clj
+++ src/alumbra/validator/selection_set/inline_spread_valid.clj
@@ -1,6 +1,29 @@
 (ns alumbra.validator.selection-set.inline-spread-valid
   (:require [invariant.core :as invariant]
-            [com.rpl.specter :refer [ALL]]))
+            [com.rpl.specter :refer [ALL]]
+            [clojure.set :as set]))
+
+;; Explanatory Text (5.4.2.3.1)
+;; ---
+;; In the scope of an object type, the only valid object type fragment spread
+;; is one that applies to the same type that is in scope.
+
+;; Explanatory Text (5.4.2.3.2)
+;; ---
+;; In scope of an object type, unions or interface spreads can be used if the
+;; object type implements the interface or is a member of the union.
+
+;; Explanatory Text (5.4.2.3.3)
+;; ---
+;; Union or interface spreads can be used within the context of an object type
+;; fragment, but only if the object type is one of the possible types of that
+;; interface or union.
+
+;; Explanatory Text (5.4.2.3.4)
+;; ---
+;; Union or interfaces fragments can be used within each other. As long as
+;; there exists at least one object type that exists in the intersection of the
+;; possible types of the scope and the spread, the spread is considered valid.
 
 ;; ## Helpers
 
@@ -15,10 +38,18 @@
          (:graphql/type-name type-condition)))
 
 (defn collect-valid-spread-types
-  [schema {:keys [analyzer/implements
-                  analyzer/implemented-by
-                  analyzer/union-types]}]
-  (set (concat implements implemented-by union-types)))
+  [{:keys [analyzer/unions]}
+   {:keys [analyzer/implements
+           analyzer/implemented-by
+           analyzer/union-types]}]
+  (let [allowed-types (set (concat implements implemented-by union-types))
+        allowed-union-types
+        (keep
+          (fn [[union-type-name union-types]]
+            (when-not (empty? (set/intersection union-types allowed-types))
+              union-type-name))
+          unions)]
+    (into allowed-types allowed-union-types)))
 
 (defn- fragment-spread-type
   [spread]
