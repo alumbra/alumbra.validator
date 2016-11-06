9d4eac5a764e60c54ce244a14a70315122c2a850
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-11-06 18:56:18 +0100

    add uniqueness invariant to fragment names.

--- src/alumbra/validator/fragments.clj
+++ src/alumbra/validator/fragments.clj
@@ -1,5 +1,6 @@
 (ns alumbra.validator.fragments
   (:require [alumbra.validator.fragments
+             [fragment-name-uniqueness :as fragment-name-uniqueness]
              [fragment-spread-target-existence :as fragment-spread-target-existence]
              [fragment-spread-type-existence :as fragment-spread-type-existence]]
             [invariant.core :as invariant]))
@@ -9,5 +10,6 @@
    GraphQL query document."
   [schema]
   (invariant/and
+    fragment-name-uniqueness/invariant
     (fragment-spread-target-existence/invariant schema)
     (fragment-spread-type-existence/invariant schema)))
---
+++ src/alumbra/validator/fragments/fragment_name_uniqueness.clj
@@ -0,0 +1,16 @@
+(ns alumbra.validator.fragments.fragment-name-uniqueness
+  (:require [invariant.core :as invariant]
+            [com.rpl.specter :refer [ALL]]))
+
+;; Formal Specification (5.4.1.1)
+;; ---
+;; - For each fragment definition `fragment` in the document
+;; - Let `fragmentName` be the name of fragment.
+;; - Let `fragments` be all fragment definitions in the document named
+;;   `fragmentName`.
+;;   - `fragments` must be a set of one.
+
+(def invariant
+  (-> (invariant/on [:graphql/fragments ALL])
+      (invariant/unique :validator/fragment-name-uniqueness
+                        {:unique-by :graphql/fragment-name})))
