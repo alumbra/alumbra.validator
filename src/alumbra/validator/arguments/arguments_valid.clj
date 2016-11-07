4b895adef7054fcfce82f6ac21275bc856e16a85
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-11-07 16:44:36 +0100

    add invariant checking nullable/required fields.

--- src/alumbra/validator/arguments/arguments_valid.clj
+++ src/alumbra/validator/arguments/arguments_valid.clj
@@ -11,17 +11,72 @@
 ;;   field or definition named `argumentName`.
 ;; - `argumentDefinition` must exist.
 
-(defn- field-arguments-invariant
+;; Formal Specification (5.3.3.2)
+;; ---
+;; - For each Field or Directive in the document.
+;; - Let `arguments` be the arguments provided by the Field or Directive.
+;; - Let `argumentDefinitions` be the set of argument definitions of that
+;;   Field or Directive.
+;; - For each `definition` in argumentDefinitions:
+;;   - Let `type` be the expected type of `definition`.
+;;   - If `type` is Non‐Null:
+;;     - Let `argumentName` be the name of `definition`.
+;;     - Let `argument` be the argument in arguments named `argumentName`
+;;     - `argument` must exist.
+;;     - Let `value` be the value of `argument`.
+;;     - `value` must not be the null literal.
+
+(defn- valid-argument-name?
   [{:keys [analyzer/arguments]}]
-  (let [valid-argument-name? (set (keys arguments))]
-    (-> (invariant/on [:graphql/arguments ALL])
-        (invariant/each
-          (-> (invariant/value
-                :validator/argument-name-in-scope
-                (comp valid-argument-name? :graphql/argument-name))
-              (invariant/with-error-context
-                (fn [_ {:keys [graphql/argument-name]}]
-                  {:analyzer/argument-name argument-name})))))))
+  (comp (set (keys arguments)) :graphql/argument-name))
+
+(defn- collect-required-arguments
+  [{:keys [analyzer/arguments]}]
+  (->> (vals arguments)
+       (filter :analyzer/non-null?)
+       (map :analyzer/argument-name)
+       (set)))
+
+(defn- argument-nullable?
+  [field]
+  (let [required-argument? (collect-required-arguments field)]
+    (fn [{:keys [graphql/argument-name
+                 graphql/argument-value]}]
+      (or (not (required-argument? argument-name))
+          (not= (:graphql/value-type argument-value) :null)))))
+
+(defn- required-arguments-invariant
+  [field]
+  (let [required-arguments (collect-required-arguments field)
+        find-missing (fn [{:keys [graphql/arguments]}]
+                       (->> (map :graphql/argument-name arguments)
+                            (reduce disj required-arguments)))]
+    (invariant/with-error-context
+      (invariant/value
+        :validator/required-non-null-arguments
+        (comp empty? find-missing))
+      (fn [_ value]
+        {:analyzer/missing-arguments
+         (find-missing value)}))))
+
+(defn- field-arguments-invariant
+  [field]
+  (-> (invariant/on-current-value)
+      (invariant/is?
+        (invariant/and
+          (required-arguments-invariant field)
+          (-> (invariant/on [:graphql/arguments ALL])
+              (invariant/each
+                (-> (invariant/and
+                      (invariant/value
+                        :validator/argument-name-in-scope
+                        (valid-argument-name? field))
+                      (invariant/value
+                        :validator/argument-nullable
+                        (argument-nullable? field)))
+                    (invariant/with-error-context
+                      (fn [_ {:keys [graphql/argument-name]}]
+                        {:analyzer/argument-name argument-name})))))))))
 
 (defn- argument-invariant
   [{:keys [analyzer/fields]}]
