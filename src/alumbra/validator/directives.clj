4ec806e387358e01a6b77f472cf7be899171486d
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-11-07 18:45:12 +0100

    add invariant on directive uniqueness.

--- src/alumbra/validator/directives.clj
+++ src/alumbra/validator/directives.clj
@@ -1,9 +1,11 @@
 (ns alumbra.validator.directives
   (:require [alumbra.validator.directives
-             [directives-defined :as directives-defined]]
+             [directives-defined :as directives-defined]
+             [directive-uniqueness :as directive-uniqueness]]
             [invariant.core :as invariant]))
 
 (defn invariant
   [schema]
   (invariant/and
+    directive-uniqueness/invariant
     (directives-defined/invariant schema)))
---
+++ src/alumbra/validator/directives/directive_uniqueness.clj
@@ -0,0 +1,21 @@
+(ns alumbra.validator.directives.directive-uniqueness
+  (:require [invariant.core :as invariant]
+            [invariant.debug :as debug]
+            [com.rpl.specter :refer :all]))
+
+;; Formal Specification (5.6.3)
+;; ---
+;; - For every `location` in the document for which Directives can apply:
+;; Let directives be the set of Directives which apply to location.
+;; For each directive in directives:
+;; Let directiveName be the name of directive.
+;; Let namedDirectives be the set of all Directives named directiveName in directives.
+;; namedDirectives must be a set of one.
+
+(def invariant
+  (-> (invariant/on [(walker :graphql/directives)])
+      (invariant/each
+        (-> (invariant/on [:graphql/directives ALL])
+            (invariant/unique
+              :validator/directive-uniqueness
+              {:unique-by :graphql/directive-name})))))
