86b865ca6150ab144f33f088db046096b452bc25
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-11-09 15:06:34 +0100

    merge introspection schema/fields when analyzing.

--- project.clj
+++ project.clj
@@ -8,8 +8,8 @@
   :dependencies [[org.clojure/clojure "1.9.0-alpha14" :scope "provided"]
                  [com.stuartsierra/dependency "0.2.0"]
                  [alumbra/spec "0.1.0-SNAPSHOT"]
+                 [alumbra/parser "0.1.0-SNAPSHOT"]
                  [invariant "0.1.0-SNAPSHOT"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]
-                                  [alumbra/parser "0.1.0-SNAPSHOT"]
                                   [alumbra/generators "0.1.0"]]}}
   :pedantic? :abort)
--- src/alumbra/analyzer.clj
+++ src/alumbra/analyzer.clj
@@ -6,12 +6,14 @@
              [schema-root :as schema-root]
              [types :as types]
              [unions :as unions]
-             [valid-fragment-spreads :as valid-fragment-spreads]
-             spec]
-            [clojure.spec :as s]
+             [valid-fragment-spreads :as valid-fragment-spreads]]
+            [alumbra.parser :as ql]
+            [clojure.java.io :as io]
             [com.rpl.specter :refer :all]))
 
-(defn analyze
+;; ## Analyzer
+
+(defn- analyze*
   "Analyze a GraphQL schema conforming to `:graphql/schema` to produce a
    more compact representation conforming to `:analyzer/schema`."
   [schema]
@@ -24,6 +26,41 @@
       (kinds/aggregate)
       (valid-fragment-spreads/aggregate)))
 
-(s/fdef analyze
-        :args (s/cat :schema :graphql/schema)
-        :ret  :analyzer/schema)
+;; ## Introspection Schema/Operations
+
+(def ^:private introspection-schema
+  (-> (io/resource "alumbra/GraphQLIntrospection.schema")
+      (slurp)
+      (ql/parse-schema)
+      (analyze*)))
+
+(def ^:private introspection-query-fields
+  (-> "type __Introspection {
+         __schema: __Schema!
+         __type(name: String!): __Type
+       }"
+      (ql/parse-schema)
+      (analyze*)
+      (get-in [:analyzer/types "__Introspection" :analyzer/fields])
+      (dissoc "__typename")))
+
+(defn- ^:private introspection-query-fields-for
+  [type-name]
+  (-> introspection-query-fields
+      (assoc-in ["__schema" :analyzer/containing-type-name] type-name)
+      (assoc-in ["__type" :analyzer/containing-type-name] type-name)))
+
+(defn add-introspection-queries
+  [{:keys [analyzer/schema-root] :as schema}]
+  (if-let [root-type (get schema-root "query")]
+    (->> (introspection-query-fields-for root-type)
+         (update-in schema [:analyzer/types root-type :analyzer/fields] merge))
+    schema))
+
+;; ## Analyzer + Introspection
+
+(defn analyze
+  [schema]
+  (->> (analyze* schema)
+       (merge-with into introspection-schema)
+       (add-introspection-queries)))
