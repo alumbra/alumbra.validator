67b8a298c495b29059a78e542bfd9d1c95d281a9
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-11-27 23:35:27 +0100

    upgrade 'alumbra.spec' dependency.
    Variable validation errors have been adjusted.

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
             :year 2016
             :key "mit"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha14" :scope "provided"]
-                 [alumbra/spec "0.1.1" :scope "provided"]
+                 [alumbra/spec "0.1.2" :scope "provided"]
                  [com.stuartsierra/dependency "0.2.0"]
                  [invariant "0.1.0"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]
