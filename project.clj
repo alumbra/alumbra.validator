c5ff0be7ba5ae6a3c8c5c26e56bdde7a7e29efb3
Yannick Scherer <yannick.scherer@stylefruits.de>
2017-02-22 17:14:57 +0100

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject alumbra/validator "0.1.0"
+(defproject alumbra/validator "0.1.1-SNAPSHOT"
   :description "Validator for GraphQL ASTs."
   :url "https://github.com/alumbra/alumbra.validator"
   :license {:name "MIT License"
