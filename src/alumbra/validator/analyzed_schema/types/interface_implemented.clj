9d81438f52402be75b6fae9532bfad63cb660e1c
Yannick Scherer <yannick.scherer@stylefruits.de>
2017-03-25 08:25:55 +0100

    implement invariant for interface field implementation.

--- src/alumbra/validator/analyzed_schema/types/interface_implemented.clj
+++ src/alumbra/validator/analyzed_schema/types/interface_implemented.clj
@@ -1,10 +1,24 @@
 (ns alumbra.validator.analyzed-schema.types.interface-implemented
   (:require [invariant.core :as invariant]
-            [com.rpl.specter :refer :all]))
+            [com.rpl.specter :refer :all]
+            [clojure.set :as set]))
 
 ;; Types implementing interfaces need to implement all the fields.
 ;; TODO: find out whether it's okay to specialize the interface.
 
+(defn- missing-fields
+  [{:keys [interfaces]} {:keys [implements fields]}]
+  (if-not (empty? implements)
+    (let [interface-fields (->> implements
+                                (mapcat (comp keys :fields interfaces))
+                                (set))
+          given-fields (set (keys fields))]
+      (set/difference interface-fields given-fields))
+    #{}))
+
 (def invariant
   (constantly
-    nil))
+    (invariant/property
+      :type/implements-all-interface-fields
+      (fn [{:keys [state/schema]} type]
+        (empty? (missing-fields schema type))))))
